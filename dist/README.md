6c70f217f360430e59852a4873f5ea3d9af745dd
Yolanda Jacobs <y.jacobs@hotmail.com>
2022-08-05 17:20:20 -0400

    generate markdown function

--- dist/README.md
+++ dist/README.md
@@ -1,2 +1,36 @@
-# title
 
+  # project title
+
+  ## Description
+
+  sample descriptionssss
+
+  ## Table of Contents
+
+  
+  - [Installation](#installation)
+  - [Questions](#questions)
+
+  
+  ## Installation
+
+  instructionsss
+  
+
+  
+
+  
+  ## License
+
+  Apache 2.0 License
+  
+
+  
+
+  
+
+  ## Questions
+
+  You can reach me with questions through email at emailaddrres
+
+  [My GitHub](https://github.com/yoli19)
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -8,12 +8,98 @@ function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return '';
+  }
+  return `
+  ## License
+
+  ${license}
+  `;
+};
+
+function generateInstallation(installation) {
+  if (!installation) {
+    return '';
+  }
+  return `
+  ## Installation
+
+  ${installation}
+  `;
+};
+
+function generateUsage(usage) {
+  if (!usage) {
+    return '';
+  }
+  return `
+  ## Usage
+
+  ${usage}
+  `;
+};
+
+function generateContributing(contributing) {
+  if (!contributing) {
+    return '';
+  }
+  return `
+  ## Contributing
+
+  ${contributing}
+  `;
+};
+
+function generateTests(tests) {
+  if (!tests) {
+    return '';
+  }
+  return `
+  ## Tests
+
+  ${tests}
+  `;
+};
+
+function generateTableOfContents(data) {
+  if (!data.installation) {
+    return '';
+  }
+  return `
+  - [Installation](#installation)`;
+};
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
 
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+
+  ${generateTableOfContents(data)}
+  - [Questions](#questions)
+
+  ${generateInstallation(data.installation)}
+
+  ${generateUsage(data.usage)}
+
+  ${renderLicenseSection(data.license)}
+
+  ${generateContributing(data.contributing)}
+
+  ${generateTests(data.tests)}
+
+  ## Questions
+
+  You can reach me with questions through email at ${data.email}
+
+  [My GitHub](https://github.com/${data.username})
 `;
 }
 
